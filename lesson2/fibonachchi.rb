14eaf769c76b3b968bb4f746fe583374e26ffd7d
req12345 <req12345@mail.ru>
2021-03-28 21:27:39 +0300

    [add] new file

---
+++ lesson2/fibonachchi.rb
@@ -0,0 +1,18 @@
+#Заполнить массив числами фибоначчи до 100
+fibo = []
+i = 0
+loop do
+  if fibo.size < 2
+    fibo << i
+    i += 1
+  else
+    i = fibo[-1] + fibo[-2]
+    if i <= 100
+      fibo << i
+  else
+    break
+  end
+end
+end
+
+puts fibo.to_s
