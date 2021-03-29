4d7b23e07dee1126f19021f2f0fa47f3f3165bc5
req12345 <req12345@mail.ru>
2021-03-29 19:27:45 +0300

    [fix] style

--- lesson2/months.rb
+++ lesson2/months.rb
@@ -6,6 +6,6 @@ months = {january: 31, february: 28, march: 31, april: 30, may: 31, june:30, jul
   august: 31, september: 30, october: 31, november: 30, december: 31}
 months.each do |month, days|
   if days == 30
-  puts month
-end
+    puts month
+  end
 end
