1fc9e163eaf0b567447b3531419d3843e48b202e
req12345 <req12345@mail.ru>
2021-03-29 19:10:14 +0300

    [fix] code

--- lesson2/alphabet.rb
+++ lesson2/alphabet.rb
@@ -1,9 +1,12 @@
 # Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
-alphabet = {
-  a: 1, e: 5, i: 9, o: 15, u: 21, y: 25, b: 2, c: 3, d: 4, f: 6, g: 7,
-  h: 8, j: 10, k: 11, l: 12, m: 13, n: 14, p: 16, q: 17, r: 18, s: 19, t: 20,
-  v: 22, w: 23, x: 24, z: 26
-}
-vowel = alphabet.select! { |let, num| [:a, :e, :i, :o, :u, :y].include?(let) }
 
-puts vowel
+alphabet = ('a'..'z').to_a
+vowel = ["a", "e", "i", "o", "u", "y"]
+result = {}
+alphabet.each do |let|
+  if vowel.include?(let)
+    result[let] = (alphabet.index(let) - 1)
+  end
+end
+
+puts result
