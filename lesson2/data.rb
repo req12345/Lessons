4fa3ec57ca58aa4d23f514f5750782b295d1ab2b
req12345 <req12345@mail.ru>
2021-03-29 19:54:16 +0300

    [fix] style

--- lesson2/data.rb
+++ lesson2/data.rb
@@ -12,10 +12,10 @@ days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days[1] = 29 if leap == true
 
 if month == 1
-    puts "Порядковый номер даты #{day}"
+  puts "Порядковый номер даты #{day}"
 else
   days[0..(month - 2)].each do |n|
-  day += n
-end
+    day += n
+  end
   puts "Порядковый номер даты #{(day)}"
 end
