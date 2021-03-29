efa7be16d5f17237303b9947a6ff14c710f067ce
req12345 <req12345@mail.ru>
2021-03-29 19:25:17 +0300

    [fix] more easy

--- lesson2/num_pitch_5.rb
+++ lesson2/num_pitch_5.rb
@@ -1,8 +1,4 @@
 #Заполнить массив числами от 10 до 100 с шагом 5
 num_pitch_5 = []
-(10..100).each do |i|
-  if i % 5 == 0
-  num_pitch_5.push(i)
-  end
-end
-puts num_pitch_5.to_s
+(10..100).step(5) {|i| num_pitch_5 << i}
+puts num_pitch_5
