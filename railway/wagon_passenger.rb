eb9f86ec52eda3438656cdf323694856ea1e1126
req12345 <req12345@mail.ru>
2021-04-04 21:30:03 +0300

    [fix]

--- railway/wagon_passenger.rb
+++ railway/wagon_passenger.rb
@@ -4,14 +4,14 @@ class WagonPassenger < TrainPassenger
     wagon = 1
   end
 
-  def hook_in_wagon_passenger
+  def attach_wagon_passenger
     return if @speed != 0
     @wagons_passenger << wagon
     wagon += 1
-      end
+    end
 
-  def hook_out_wagon_passenger
-    return unless @wagons_passenger.empty?
+  def detach_wagon_passenger
+    return unless @wagons_passenger.empty? == true || speed == 0
     @wagons_passenger.delete_at(-1)
   end
 
