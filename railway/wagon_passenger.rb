4fdf97c9751e55ec986df769071f52871350d55e
req12345 <req12345@mail.ru>
2021-04-04 20:00:39 +0300

    [fix]

--- railway/wagon_passenger.rb
+++ railway/wagon_passenger.rb
@@ -1,18 +1,18 @@
 class WagonPassenger < TrainPassenger
 
-  def inuitialize(amount)
+  def inuitialize
     @amount = 1
   end
 
   def hook_in_wagon_passenger
     return if @speed != 0
-    
+    @wagons_passenger << @amount
     @amount += 1
   end
 
   def hook_out_wagon_passenger
-    return unless @wagons > 1
-    @wagons -= 1
+    return unless @amount > 1
+    @amount -= 1
   end
 
 end
