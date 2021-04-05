cffb4b6e3b37c23a749fdc837df9659596ca8326
req12345 <req12345@mail.ru>
2021-04-06 00:14:13 +0300

    [fix]

--- railway/wagon_cargo.rb
+++ railway/wagon_cargo.rb
@@ -1,17 +1,6 @@
-class WagonCargo < TrainCargo
+class WagonCargo < Wagon
 
-  def initialize(wagon)
-    wagon = 1
-  end
-
-  def attach_wagon_cargo
-    return if @speed != 0
-    @wagons_cargo << wagon
-    wagon += 1
-    end
-
-  def detach_wagon_cargo
-    return unless @wagons_cargo.empty? == true || speed == 0
-    @wagons_cargo.delete_at(-1)
+  def initialize
+    @type = 'cargo'
   end
 end
