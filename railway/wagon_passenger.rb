8a3b1ade4e0e3b7b383fe87afea9becb72d9ecc0
req12345 <req12345@mail.ru>
2021-04-06 00:14:30 +0300

    [fix]

--- railway/wagon_passenger.rb
+++ railway/wagon_passenger.rb
@@ -1,18 +1,6 @@
-class WagonPassenger < TrainPassenger
+class WagonPassenger < Wagon
 
-  def initialize(wagon)
-    wagon = 1
+  def initialize
+    @type = 'passanger'
   end
-
-  def attach_wagon_passenger
-    return if @speed != 0
-    @wagons_passenger << wagon
-    wagon += 1
-    end
-
-  def detach_wagon_passenger
-    return unless @wagons_passenger.empty? == true || speed == 0
-    @wagons_passenger.delete_at(-1)
-  end
-
 end
