1a2ec9f490ae89b12070559252508e8f1ed27a70
req12345 <req12345@mail.ru>
2021-05-07 22:11:23 +0300

    [fix]

--- railway/accessors.rb
+++ railway/accessors.rb
@@ -1,4 +1,12 @@
 module Accessors
+  class << self
+    def self.included(base)
+      base.extend ClassMethods
+    end
+  end
+
+  module ClassMethods
+
   def attr_accessor_with_history(*attr)
     attr.each do |attr|
       var_name = "@#{attr}".to_sym
@@ -16,21 +24,23 @@ module Accessors
   end
 
   def strong_attr_accessor(attr_name, attr_class)
-    var_name = "@#{attr_name}".to_sym
 
-    define_method(var_name) { instance_variable_get(var_name) }
+    define_method(attr_name.to_sym) { instance_variable_get("@#{attr_name}".to_s) }
 
-    define_method(var_name) do |value|
+    define_method("#{attr_name}=") do |value|
       raise TypeError, "Type should be #{attr_class}" unless value.is_a?(attr_class)
-      instance_variable_set("@#{var_name}")
+
+      instance_variable_set("@#{attr_name}", value)
       end
     end
+  end
 end
 
-
 class Test
-  extend Accessors
+  include Accessors
 
   attr_accessor_with_history :a, :b
   strong_attr_accessor :c, :d
 end
+
+t = Test.new
