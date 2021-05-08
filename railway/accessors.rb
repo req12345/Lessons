dd4c521cb19206e2b5f9f603c419885a4f9342fa
req12345 <req12345@mail.ru>
2021-05-08 15:54:26 +0300

    [fxs]

--- railway/accessors.rb
+++ railway/accessors.rb
@@ -1,36 +1,34 @@
 module Accessors
-  class << self
-    def self.included(base)
-      base.extend ClassMethods
-    end
+  def self.included(base)
+   base.extend(ClassMethods)
   end
 
   module ClassMethods
 
-  def attr_accessor_with_history(*attr)
-    attr.each do |attr|
-      var_name = "@#{attr}".to_sym
+    def attr_accessor_with_history(*attr)
+      attr.each do |attr|
+        var_name = "@#{attr}".to_sym
 
-      define_method(attr) { instance_variable_get(var_name) }
+        define_method(attr) { instance_variable_get(var_name) }
 
-      define_method("#{attr}_history") { instance_variable_get("@#{attr}_history") }
+        define_method("#{attr}_history") { instance_variable_get("@#{attr}_history") }
 
-      define_method("#{attr}=".to_sym) do |value|
-        instance_variable_set(var_name, value)
-        history = instance_variable_get("@#{attr}_history") || []
-        instance_variable_set("@#{attr}_history", history << value)
+        define_method("#{attr}=".to_sym) do |value|
+          instance_variable_set(var_name, value)
+          history = instance_variable_get("@#{attr}_history") || []
+          instance_variable_set("@#{attr}_history", history << value)
+        end
       end
     end
-  end
 
-  def strong_attr_accessor(attr_name, attr_class)
+    def strong_attr_accessor(attr_name, attr_class)
 
-    define_method(attr_name.to_sym) { instance_variable_get("@#{attr_name}".to_s) }
+      define_method(attr_name) { instance_variable_get("@#{attr_name}".to_sym) }
 
-    define_method("#{attr_name}=") do |value|
-      raise TypeError, "Type should be #{attr_class}" unless value.is_a?(attr_class)
+      define_method("#{attr_name}=".to_sym) do |value|
+        raise TypeError, "Type should be #{attr_class}" unless value.is_a?(attr_class.class)
 
-      instance_variable_set("@#{attr_name}", value)
+        instance_variable_set("@#{attr_name}", value)
       end
     end
   end
@@ -42,5 +40,3 @@ class Test
   attr_accessor_with_history :a, :b
   strong_attr_accessor :c, :d
 end
-
-t = Test.new
